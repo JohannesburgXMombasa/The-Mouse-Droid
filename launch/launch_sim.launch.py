0bcd03782256fdbf8183bd7648009020058ac5d1
JohannesburgXMombasa <calebj.richie@calbaptist.edu>
2023-03-25 19:07:09 -0700

    new file:   config/gazebo_params.yaml 	modified:   description/lidar.xacro 	modified:   launch/launch_sim.launch.py

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,14 +1,15 @@
+
 import os
- 
+
 from ament_index_python.packages import get_package_share_directory
- 
- 
+
+
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
+from launch.event_handlers import OnProcessExit
 from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessStart
+
 
 from launch_ros.actions import Node
  
@@ -25,13 +26,16 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
- 
+    
+    
     # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
  
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -40,11 +44,29 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
  
- 
+    joint_state_broadcaster_spawner = Node(package="controller_manager",
+                                          executable="spawner",
+                                          arguments=["joint_state_broadcaster",
+                                          "--controller-manager", "/controller_manager"],
+                                          )
+    
+    robot_controller_spawner = Node(package="controller_manager",
+                                    executable="spawner",
+                                    arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+                                    )
+
+    delayed_robot_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
  
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        joint_state_broadcaster_spawner,
+        delayed_robot_controller_spawner,
     ])
